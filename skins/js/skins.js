b8f2c2ca307cf140549d5359b115aede0846abe2
暗组夏天 <1005584676@qq.com>
2018-06-27 20:28:30 +0800

    Update skins.js

--- skins/js/skins.js
+++ skins/js/skins.js
@@ -14,6 +14,21 @@ var skins = [
         "it": "https://1005584676.github.io/skins/res/it/墨绿安卓风.th",
         "rid": "8904"
 	}, 
+       {
+        "id": "qsf",
+        "name": "浅色风风",
+        "author": "胖虎",
+        "size": "449KB",
+        "description": "暂定",
+        "update": "",
+        "star": "4",
+        "type": "",
+        "lables":  ["设计创意", "可爱风", "河图岁寒", "无边框", "亮色色", "透明"],
+        "time": "2018-5-19",
+        "imgs": ["http://pa77l2znq.bkt.clouddn.com/18-6-26/53687947.jpg", "http://pa77l2znq.bkt.clouddn.com/18-6-26/53687947.jpg", "http://pa77l2znq.bkt.clouddn.com/18-6-26/39955372.jpg", "http://pa77l2znq.bkt.clouddn.com/18-6-26/82861557.jpg", "http://pa77l2znq.bkt.clouddn.com/18-6-26/75231534.jpg"],
+        "it": "https://1005584676.github.io/skins/res/it/浅色风.th",
+        "rid": "8904"
+	}, 
      {
         "id": "azjj",
         "name": "AZ极简",
