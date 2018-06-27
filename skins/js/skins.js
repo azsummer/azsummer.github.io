af9853d9d00f0790e6caa7ff8009d6ddfc457779
暗组夏天 <1005584676@qq.com>
2018-06-27 20:36:40 +0800

    Update skins.js

--- skins/js/skins.js
+++ skins/js/skins.js
@@ -21,10 +21,10 @@ var skins = [
         "size": "449KB",
         "description": "暂定",
         "update": "",
-        "star": "4",
+        "star": "5",
         "type": "",
         "lables":  ["设计创意", "可爱风", "河图岁寒", "无边框", "亮色色", "透明"],
-        "time": "2018-5-19",
+        "time": "2018-6-27",
         "imgs": ["http://pa77l2znq.bkt.clouddn.com/18-6-26/53687947.jpg", "http://pa77l2znq.bkt.clouddn.com/18-6-26/53687947.jpg", "http://pa77l2znq.bkt.clouddn.com/18-6-26/39955372.jpg"],
         "it": "https://1005584676.github.io/skins/res/it/浅色风.th",
         "rid": "8904"
